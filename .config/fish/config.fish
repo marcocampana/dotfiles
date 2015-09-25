0317e8c5b1050542b0c9016c58463ab61b93d6ec
Marco Campana <marco.campana@fundingcircle.com>
2015-09-25 15:18:06 +0100

    Add Fish shell config and functions

---
+++ .config/fish/config.fish
@@ -0,0 +1,28 @@
+alias ls='ls -G'
+alias ..='cd ..'
+alias l='ls -la'
+
+alias pg='ps aux | grep'
+
+alias s='subl'
+
+# git aliases
+alias g='git'
+alias gs='git status'
+alias gd='git diff'
+alias gm='git commit -m'
+alias gma='git commit -am'
+alias gb='git branch'
+alias gc='git checkout'
+alias gra='git remote add'
+alias grr='git remote rm'
+alias gpu='git pull --rebase'
+
+alias gh='github'
+
+# rails 3.x aliases
+alias rc='rails c'
+alias rs='rails s'
+alias be='bundle exec'
+
+rvm default
