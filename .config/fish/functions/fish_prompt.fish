61812ff78d3c662c37e381cd396806054358cd5d
Marco Campana <marco.campana@fundingcircle.com>
2015-10-05 18:00:22 +0100

    Add powerline-shell script

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,54 +1,4 @@
 function fish_prompt --description 'Write out the prompt'
-	# set -l last_status $status
-	# set -l git_prompt_color red
-	# set -l time_color yellow
-
-	# # # Just calculate this once, to save a few cycles when displaying the prompt
-	# if not set -q __fish_prompt_hostname
-	# 	set -g __fish_prompt_hostname (hostname|cut -d . -f 1)
-	# end
-
-	# set -l normal (set_color normal)
-
-	# # Hack; fish_config only copies the fish_prompt function (see #736)
-	# if not set -q -g __fish_classic_git_functions_defined
-	# 	set -g __fish_classic_git_functions_defined
-
-	# 	function __fish_repaint_user --on-variable fish_color_user --description "Event handler, repaint when fish_color_user changes"
-	# 		if status --is-interactive
-	# 			commandline -f repaint ^/dev/null
-	# 		end
-	# 	end
-
-	# 	function __fish_repaint_host --on-variable fish_color_host --description "Event handler, repaint when fish_color_host changes"
-	# 		if status --is-interactive
-	# 			commandline -f repaint ^/dev/null
-	# 		end
-	# 	end
-
-	# 	function __fish_repaint_status --on-variable fish_color_status --description "Event handler; repaint when fish_color_status changes"
-	# 		if status --is-interactive
-	# 			commandline -f repaint ^/dev/null
-	# 		end
-	# 	end
-
-	# 	function __fish_repaint_bind_mode --on-variable fish_key_bindings --description "Event handler; repaint when fish_key_bindings changes"
-	# 		if status --is-interactive
-	# 			commandline -f repaint ^/dev/null
-	# 		end
-	# 	end
-
-	# 	# initialize our new variables
-	# 	if not set -q __fish_classic_git_prompt_initialized
-	# 		set -qU fish_color_user; or set -U fish_color_user -o green
-	# 		set -qU fish_color_host; or set -U fish_color_host -o cyan
-	# 		set -qU fish_color_status; or set -U fish_color_status red
-	# 		set -U __fish_classic_git_prompt_initialized
-	# 	end
-	# end
-
-	# set -l color_cwd cyan
-	# set -l prefix
 	switch $USER
 	case root toor
 		set suffix '#'
@@ -56,36 +6,7 @@ function fish_prompt --description 'Write out the prompt'
 		set suffix '→'
 	end
 
-	# set -l prompt_status
-	# if test $last_status -ne 0
-	# 	set prompt_status ' ' (set_color $fish_color_status) "[$last_status]" "$normal"
-	# end
-
-	# set -l mode_str
-	# switch "$fish_key_bindings"
-	# case '*_vi_*' '*_vi'
-	# 	# possibly fish_vi_key_bindings, or custom key bindings
-	# 	# that includes the name "vi"
-	# 	set mode_str (
-	# 		echo -n " "
-	# 		switch $fish_bind_mode
-	# 		case default
-	# 			set_color --bold --background red white
-	# 			echo -n "[N]"
-	# 		case insert
-	# 			set_color --bold green
-	# 			echo -n "[I]"
-	# 		case visual
-	# 			set_color --bold magenta
-	# 			echo -n "[V]"
-	# 		end
-	# 		set_color normal
-	# 	)
-	# end
-
-	# echo -s "  " (set_color $time_color) [(date +%H:%M:%S)] " " $normal "$USER " $normal @ "$__fish_prompt_hostname"
-	# echo -n -s →  $normal ' ' (set_color $color_cwd) (prompt_pwd) $normal (set_color $git_prompt_color) (__fish_git_prompt) $normal $prompt_status "$mode_str" " "
-	~/powerline-shell.py $status --shell bare ^/dev/null
+	~/dotfiles/powerline-shell.py $status --shell bare ^/dev/null
 	printf "\n"
 	printf $suffix
 	printf " "
